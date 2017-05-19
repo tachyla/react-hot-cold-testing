45caabc9a70eb771e3c5bb173f61582be133db2e
tachyla <tachylad.murray@gmail.com>
2017-05-19 10:19:07 -0500

    all smoke test complete

---
+++ src/components/smoke.test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Game from './game';
+import GuessSection from './guess-section';
+import GuessCount from './guess-count';
+import GuessList from './guess-list';
+import GameForm from './guess-form';
+import Header from './header';
+import InfoModal from './info-modal';
+import TopNav from './top-nav';
+
+describe('<Game/>', () => {
+    it('Smoke Test for Game', () => {
+        shallow(<Game />);
+    })
+} )
+
+describe('<GuessSection/>', () => {
+    it('Smoke Test for GuessSection', () => {
+        shallow(<Game />);
+    })
+} )
+
+describe('<GuessCount/>', () => {
+    it('Smoke Test for GuessCount', () => {
+        shallow(<Game />);
+    })
+} )
+
+describe('<GuessList/>', () => {
+    it('Smoke Test for GuessList', () => {
+        shallow(<Game />);
+    })
+} )
+
+describe('<GameForm/>', () => {
+    it('Smoke Test for GameForm', () => {
+        shallow(<Game />);
+    })
+} )
+
+describe('<Header/>', () => {
+    it('Smoke Test for Header', () => {
+        shallow(<Game />);
+    })
+} )
+
+describe('<InfoModal/>', () => {
+    it('Smoke Test for InfoModal', () => {
+        shallow(<Game />);
+    })
+} )
+
+describe('<TopNav/>', () => {
+    it('Smoke Test for TopNav', () => {
+        shallow(<Game />);
+    })
+} )
